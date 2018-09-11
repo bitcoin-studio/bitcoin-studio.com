f28fc4fcd58ed694a4f2e4ba8b01bb83a084ec89
Janaka-Steph <rochestephane@yahoo.fr>
2018-09-11 16:17:06 +0100

    Add illustrations homepage

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,15 @@ class App extends React.Component {
         <Main/>
 
         <Footer/>
+
+        <div id={'copyrights'}>
+          <div className="strike"></div>
+          <p>Bitcoin Studio - 2018</p>
+          <p>Icons made by <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a> from <a
+            href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a
+            href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0
+            BY</a></p>
+        </div>
       </div>
     )
   }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,10 +8,12 @@ class Home extends React.Component {
         <div id={'hero-homePage'} className={'hero'}>
           <h1>BITCOIN STUDIO #1</h1>
           <p>
-            Expertise on blockchain tech, particularly Bitcoin. A network of real experts, design studio, web/interface
-            that blah blah blah. In all seriousness, we can pretty much write whatever we want here and chances are,
-            nobody will notice. The itsy bitsy spider went up the water spout. Down came the rain, and washed the spider
-            out. See what we mean? Scroll down and find out why this happens...
+            Bitcoin company based in the beautiful city of Lisbon. We do software development, workshops and in-house
+            trainings, focused on bitcoin technology and quality web development. Want to discover Bitcoin, or choose to
+            integrate Bitcoin to your sales?
+            Need to refresh your website with a new user interface, experimenting with the power of digital money?
+            Bitcoin Studio, a network of experts from a variety of backgrounds and driven by the same ideals, is here
+            for you.
           </p>
         </div>
 
@@ -34,28 +36,21 @@ class Home extends React.Component {
           <h2>THE BEST BITCOIN STUDIO</h2>
           <p>
             Fascinated by the idea and realisation of a digital, decentralized, open platform, open source,
-            censorship-resistant money,
-            we at Bitcoin Studio have a strong will to take part of this revolution.
-            The company is mainly composed by Stéphane Roche, blockchain developer (mainly Bitcoin and Ethereum),
-            then team can be constituted depending on the needs.
+            censorship-resistant money, we at Bitcoin Studio have a strong will to take part of this revolution.
+            The company is led, and mainly composed, let's face it, by Stéphane Roche, blockchain developer (mainly
+            Bitcoin and Ethereum). Depending on your needs, a team can be made.
           </p>
 
-          <p>Consectetur adipisicing elit. Aliquid consequuntur dicta
-            expedita magni minus
-            numquam perspiciatis quod sint sunt vero? Laboriosam non pariatur repellat repellendus similique. Inventore
-            nemo quidem vitae. </p>
-
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequuntur dicta expedita magni minus
-            numquam perspiciatis quod sint sunt vero? Laboriosam non pariatur repellat repellendus similique. Inventore
-            nemo quidem vitae. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequuntur dicta
-            expedita magni minus
-            numquam perspiciatis quod sint sunt vero? Laboriosam non pariatur repellat repellendus similique. Inventore
-            nemo quidem vitae. </p>
+          <p>
+            If you look around you, there is more and more individuals paying attention to this strange, untangible
+            money. Many are laughing at it also! This is not a problem. We know why we are here. This technology is
+            certainly the biggest revolution of our century and we want to be part of it.
+          </p>
         </section>
 
         <section id={'servicesDetails-homePage'}>
           <div className={'serviceItem'}>
-            <img src="https://loremflickr.com/200/150" alt="img22"/>
+            <img src="assets/icons/software_512.png" alt="software development"/>
             <p>
               <span><em>Software development</em></span>
               Bitcoin Studio reconcile Bitcoin with web development (mainly Typescript, React, Redux), offering custom
@@ -66,7 +61,8 @@ class Home extends React.Component {
 
 
           <div className={'serviceItem'}>
-            <img src="https://loremflickr.com/200/150" alt="img22"/>
+            {/*<img src="assets/icons/icons8-presentation-64.png" alt="workshops"/>*/}
+            <img src="assets/icons/education_512.png" alt="workshops"/>
             <p>
               <span><em>Workshops</em></span>
               We are offering three Bitcoin trainings, Practical, Essentials and In-depth, each of them is gradually
@@ -76,7 +72,7 @@ class Home extends React.Component {
           </div>
 
           <div className={'serviceItem'}>
-            <img src="https://loremflickr.com/200/150" alt="img22"/>
+            <img src="assets/icons/thought_512.png" alt="in-house trainings"/>
             <p>
               <span><em>In-house trainings</em></span>
               Bitcoin trainings can be given directly at your office, tailored to your specific needs.
@@ -85,7 +81,7 @@ class Home extends React.Component {
           </div>
 
           <div className={'serviceItem'}>
-            <img src="https://loremflickr.com/200/150" alt="img22"/>
+            <img src="assets/icons/consulting_512.png" alt="private consulting"/>
             <p>
               <span><em>Private consulting</em></span>
               Bitcoin Studio can respond to your specific needs.
@@ -94,10 +90,11 @@ class Home extends React.Component {
           </div>
 
           <div className={'serviceItem'}>
-            <img src="https://loremflickr.com/200/150" alt="img22"/>
+            <img src="assets/icons/hosting_512.png" alt="node as a service"/>
             <p>
               <span><em>Node as a Service</em></span>
-              Partnering with the French company <a href="https://www.adapp.tech/" target={"_blank"}>ADAPP</a>, we host and manage all kind of nodes (full nodes, LN nodes,
+              Partnering with the French company <a href="https://www.adapp.tech/" target={'_blank'}>ADAPP</a>, we host
+              and manage all kind of nodes (full nodes, LN nodes,
               BTCpay).
               Our unique deployment stack allows for easy setup, safety and a production-ready infrastructure.
             </p>
