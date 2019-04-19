1e93609ffff683dc4adf0f6e03dccd482a34c41f
Janaka-Steph <rochestephane@yahoo.fr>
2019-04-19 14:15:26 +0100

    remove test

--- react-ui/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import About from './pages/About';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<About />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
