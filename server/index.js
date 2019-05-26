1d9476624d1d30795087a6c738559355f9bad0ee
Janaka-Steph <rochestephane@yahoo.fr>
2019-05-26 19:08:34 +0100

    change port server

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 const forceDomain = require('forcedomain');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Multi-process to utilize all CPU cores.
 if (cluster.isMaster) {
