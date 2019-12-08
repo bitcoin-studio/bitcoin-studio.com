68fae4a8495a2fbdbf74679ef88ab038ecb16806
Janaka-Steph <rochestephane@yahoo.fr>
2019-12-08 23:24:44 +0000

    try prevent glitch

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -1,20 +1,24 @@
 import * as React from 'react'
 import {Route, Switch} from 'fuse-react'
+import {TailSpin} from 'svg-loaders-react'
 
 class Main extends React.Component {
+
   render() {
     return (
       <main>
-        <React.Suspense fallback={<div>Loading...</div>}>
-          <Switch>
-            <Route exact path='/' component={async () => import('../pages/Home')}/>
-            <Route exact path='/workshops' component={async () => import('../pages/Workshops')}/>
-            <Route exact path='/resources' component={async () => import('../pages/Resources')}/>
-            <Route exact path='/about' component={async () => import('../pages/About')}/>
-            <Route exact path='/contact' component={async () => import('../pages/Contact')}/>
-            <Route path='' component={async () => import('../pages/NotFound')}/>
-          </Switch>
-        </React.Suspense>
+        <Switch placeholder={
+          <div className={'loader'}>
+            <TailSpin viewBox="0 0 50 50"/>
+          </div>}
+        >
+          <Route exact path='/' component={async () => import('../pages/Home')}/>
+          <Route exact path='/workshops' component={async () => import('../pages/Workshops')}/>
+          <Route exact path='/resources' component={async () => import('../pages/Resources')}/>
+          <Route exact path='/about' component={async () => import('../pages/About')}/>
+          <Route exact path='/contact' component={async () => import('../pages/Contact')}/>
+          <Route path='' component={async () => import('../pages/NotFound')}/>
+        </Switch>
       </main>
     )
   }
