f3acb274575db26621cf46af558912a6123c9cf7
Janaka-Steph <rochestephane@yahoo.fr>
2019-12-08 22:00:20 +0000

    try prevent glitch

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -5,14 +5,16 @@ class Main extends React.Component {
   render() {
     return (
       <main>
-        <Switch>
-          <Route exact path='/' component={async () => import('../pages/Home')}/>
-          <Route exact path='/workshops' component={async () => import('../pages/Workshops')}/>
-          <Route exact path='/resources' component={async () => import('../pages/Resources')}/>
-          <Route exact path='/about' component={async () => import('../pages/About')}/>
-          <Route exact path='/contact' component={async () => import('../pages/Contact')}/>
-          <Route path='' component={async () => import('../pages/NotFound')}/>
-        </Switch>
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path='/' component={async () => import('../pages/Home')}/>
+            <Route exact path='/workshops' component={async () => import('../pages/Workshops')}/>
+            <Route exact path='/resources' component={async () => import('../pages/Resources')}/>
+            <Route exact path='/about' component={async () => import('../pages/About')}/>
+            <Route exact path='/contact' component={async () => import('../pages/Contact')}/>
+            <Route path='' component={async () => import('../pages/NotFound')}/>
+          </Switch>
+        </React.Suspense>
       </main>
     )
   }
