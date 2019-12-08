873878894d9bae498bf27908853219a517a47359
Janaka-Steph <rochestephane@yahoo.fr>
2019-12-08 18:06:16 +0000

    Add 'bitcoin test wallets generator' project

--- client/src/pages/Resources.tsx
+++ client/src/pages/Resources.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
-import { withNamespaces, Trans } from 'react-i18next'
+import {withNamespaces, Trans} from 'react-i18next'
 
 interface IProps {
   t: ((string) => string)
 }
+
 interface IState {}
 
 class Resources extends React.Component<IProps, IState> {
@@ -17,70 +18,69 @@ class Resources extends React.Component<IProps, IState> {
         </div>
 
         <section id={'projects'}>
-          <div className="hr"><hr/></div>
+          <div className="hr">
+            <hr/>
+          </div>
           <h2>{t('projects.bitcoinJS.title')}</h2>
           <p>
-            <Trans i18nKey={"projects.bitcoinJS.p1"}>
-              FAKE TEXT BLA BLA If you are interested in learning Bitcoin programming using Javascript, you should definitely check out the
-              open-source guide we wrote on BitcoinJS. We present how to perform all of the various types of transaction. This guide is
-              sufficiently detailed to be accessible to new developers. <br/>
-              <a href="https://github.com/bitcoin-studio/Bitcoin-Programming-with-BitcoinJS" target={"_blank"}>➱ Github Repository: Bitcoin Programming with BitcoinJS</a>
+            <Trans i18nKey={'projects.bitcoinJS.p1'}>
+              0 <br/>
+              <a href="https://github.com/bitcoin-studio/Bitcoin-Programming-with-BitcoinJS" target={'_blank'}>➱ xxx</a>
               <br/>
-              <a href="https://bitcoinjs-guide.bitcoin-studio.com" target={"_blank"}>➱ Book: Bitcoin Programming with BitcoinJS</a>
+              <a href="https://bitcoinjs-guide.bitcoin-studio.com" target={'_blank'}>➱ xxx</a>
             </Trans>
           </p>
 
-          <h2 className={'mt45'}>{t('projects.bitcoin-vm.title')}</h2>
+          <h2 className={'mt55'}>{t('projects.bitcointestwallets.title')}</h2>
           <p>
-            <Trans i18nKey={"projects.bitcoin-vm.p1"}>
-              FAKE TEXT BLA BLA This tool is made of three technologies, Packer, Vagrant and Ansible, allowing us to generate an Ubuntu
-              18.10 VirtualBox image and a Vagrant box packaged with softwares for Bitcoin / Lightning Network.
-              You can find Bitcoin Core, LND, c-lightning, Electrum, the BitcoinJS guide above, etc. <br/>
-              The image is ideal for Bitcoin workshops, hackathons, etc. <br/>
-              <a href="https://app.vagrantup.com/bitcoin-studio/boxes/Bitcoin-VirtualMachine" target={"_blank"}>➱ Vagrant box</a><br/>
-              <a href="https://keybase.pub/janakasteph/Bitcoin-VirtualMachine" target={"_blank"}>➱ VirtualBox image</a><br/>
-              <a href="https://github.com/bitcoin-studio/Bitcoin-VirtualMachine" target={"_blank"}>➱ Github Repository</a>
+            <Trans i18nKey={'projects.bitcointestwallets.p1'}>
+              0 <br/>
+              <a href="https://github.com/bitcoin-studio/Bitcoin-Test-Wallets-Generator" target={'_blank'}>➱ xxx</a>
             </Trans>
           </p>
 
-          <h2 className={'mt45'}>{t('projects.mnemonic.title')}</h2>
+          <h2 className={'mt55'}>{t('projects.bitcoin-vm.title')}</h2>
           <p>
-            <Trans i18nKey={"projects.mnemonic.p1"}>
-              FAKE TEXT BLA BLA A handy template to write down your mnemonic code (24 words back up). The idea behind it is to have a document that gathers
-              all the necessary information you need to be able to recover coins.
-              In particular, it prompts the user to fill in the derivation path, assuming the user knows or will search for it.
-              Having the right mnemonic code, but missing the derivation path does not permit coin recovery might make it very difficult to recover coins.
-              Finally, this template got its inspiration from the reading of Pamela Morgan <a href="https://empoweredlaw.com/books/" target={"_blank"}>Cryptoasset Inheritance Planning</a> book.
-              Bitcoin owners should make sure that the funds are redeemable by their heirs in the eventuality of their death.
-              <br/>
-              <a href={'assets/bitcoin_mnemonic_code_template.pdf'} download>➱ Bitcoin Mnemonic Code Template</a>
+            <Trans i18nKey={'projects.bitcoin-vm.p1'}>
+              0 <br/>
+              2 <br/>
+              <a href="https://app.vagrantup.com/bitcoin-studio/boxes/Bitcoin-VirtualMachine" target={'_blank'}>➱ Vagrant box</a><br/>
+              <a href="https://keybase.pub/janakasteph/Bitcoin-VirtualMachine" target={'_blank'}>➱ VirtualBox image</a><br/>
+              <a href="https://github.com/bitcoin-studio/Bitcoin-VirtualMachine" target={'_blank'}>➱ Github Repository</a>
             </Trans>
           </p>
 
-          <h2 className={'mt45'}>{t('projects.utxo.title')}</h2>
+          <h2 className={'mt55'}>{t('projects.mnemonic.title')}</h2>
           <p>
-            <Trans i18nKey={"projects.utxo.p1"}>
-              FAKE TEXT BLA BLA
-              <br/>
-              <a href={'assets/transactions_utxo_model.pdf'} download>➱ Bitcoin Transactions Templates</a>
+            <Trans i18nKey={'projects.mnemonic.p1'}>
+              0 <a href="https://empoweredlaw.com/books/" target={'_blank'}>xxx</a>
+              2 <br/>
+              <a href={'assets/bitcoin_mnemonic_code_template.pdf'} download>➱ xxx</a>
             </Trans>
           </p>
 
-          <h2 className={'mt45'}>{t('projects.nespressoLN.title')}</h2>
+          <h2 className={'mt55'}>{t('projects.utxo.title')}</h2>
+          <p>
+            <Trans i18nKey={'projects.utxo.p1'}>
+              0 <br/>
+              <a href={'assets/transactions_utxo_model.pdf'} download>➱ xxx</a>
+            </Trans>
+          </p>
+
+          <h2 className={'mt55'}>{t('projects.nespressoLN.title')}</h2>
           <div id={'nespressoLightningDesc'}>
             <p>
-              <Trans i18nKey={"projects.nespressoLN.p1"}>
-                FAKE TEXT BLA BLA
-                <a href="https://www.elecrow.com/wiki/index.php?title=ESP8266_IOT_Board(Arduino_IDE_or_NodeMCU_Lua_Programming)" target={"_blank"}>ESP8266 --</a>
-                FAKE TEXT BLA BLA
-                <a href="http://theblock.cafe/" target={"_blank"}>The Block --</a>
-                FAKE TEXT BLA BLA
-                <br/>
-                <a href="https://coffee.bitcoin-studio.com" target={"_blank"}>➱ The custom user interface</a><br/>
-                <a href="https://github.com/bitcoin-studio/LightningNespressoUI" target={"_blank"}>➱ The custom user interface code</a><br/>
-                <a href="https://github.com/bitcoin-studio/LightningNespressoServer" target={"_blank"}>➱ The code that runs on the board</a><br/>
-                <a href="https://www.thingiverse.com/thing:3772726" target={"_blank"}>➱ CAD Files on Thingiverse</a><br/>
-                <a href="https://www.yoctopuce.com/EN/article/an-automatic-nespresso-capsule-dispenser" target={"_blank"}>➱ More info about the original machine</a>
+              <Trans i18nKey={'projects.nespressoLN.p1'}>
+                0 <a href="https://www.elecrow.com/wiki/index.php?title=ESP8266_IOT_Board(Arduino_IDE_or_NodeMCU_Lua_Programming)"
+                     target={'_blank'}>xxx</a>
+                2 <a href="http://theblock.cafe/" target={'_blank'}>xxx</a>
+                4 <br/>
+                <a href="https://coffee.bitcoin-studio.com" target={'_blank'}>➱ The custom user interface</a><br/>
+                <a href="https://github.com/bitcoin-studio/LightningNespressoUI" target={'_blank'}>➱ The custom user interface code</a><br/>
+                <a href="https://github.com/bitcoin-studio/LightningNespressoServer" target={'_blank'}>➱ The code that runs on the board</a><br/>
+                <a href="https://www.thingiverse.com/thing:3772726" target={'_blank'}>➱ CAD Files on Thingiverse</a><br/>
+                <a href="https://www.yoctopuce.com/EN/article/an-automatic-nespresso-capsule-dispenser" target={'_blank'}>➱ More info about
+                  the original machine</a>
               </Trans>
             </p>
             <video
@@ -89,7 +89,8 @@ class Resources extends React.Component<IProps, IState> {
               id={'NespressoLightningVideo'}
               poster={'assets/video/LN_Nespresso_Vending_Machine_Thumbnail.jpg'}
               src={'assets/video/LN_Nespresso_Vending_Machine.mp4'}
-            >Sorry, this video cannot be played in your current browser</video>
+            >Sorry, this video cannot be played in your current browser
+            </video>
 
             <p id={'NespressoLightningPrice'}>
               <span id={'NespressoLightningPrice1'}>{t('projects.nespressoLN.price1')}</span>
@@ -99,7 +100,9 @@ class Resources extends React.Component<IProps, IState> {
         </section>
 
         <section id={'slides'}>
-          <div className="hr"><hr/></div>
+          <div className="hr">
+            <hr/>
+          </div>
           <h2>{t('slides.title')}</h2>
 
           <div className={'slideItemContainer2'}>
@@ -152,9 +155,9 @@ class Resources extends React.Component<IProps, IState> {
                 data={'assets/education/keys_addresses_wallets_bitcoin-studio.pdf'}/>
             </div>
             <div className="slideButton">
-            <a href={'assets/education/keys_addresses_wallets_bitcoin-studio.pdf'} download>
-              {t('slides.download')}
-            </a>
+              <a href={'assets/education/keys_addresses_wallets_bitcoin-studio.pdf'} download>
+                {t('slides.download')}
+              </a>
             </div>
           </div>
 
@@ -166,9 +169,9 @@ class Resources extends React.Component<IProps, IState> {
                 data={'assets/education/mining_consensus_forks_bitcoin-studio.pdf'}/>
             </div>
             <div className="slideButton">
-            <a href={'assets/education/mining_consensus_forks_bitcoin-studio.pdf'} download>
-              {t('slides.download')}
-            </a>
+              <a href={'assets/education/mining_consensus_forks_bitcoin-studio.pdf'} download>
+                {t('slides.download')}
+              </a>
             </div>
           </div>
 
@@ -181,9 +184,9 @@ class Resources extends React.Component<IProps, IState> {
                 data={'assets/education/bitcoin_transaction_bitcoin-studio.pdf'}/>
             </div>
             <div className="slideButton">
-            <a href={'assets/education/bitcoin_transaction_bitcoin-studio.pdf'} download>
-              {t('slides.download')}
-            </a>
+              <a href={'assets/education/bitcoin_transaction_bitcoin-studio.pdf'} download>
+                {t('slides.download')}
+              </a>
             </div>
           </div>
 
@@ -275,10 +278,10 @@ class Resources extends React.Component<IProps, IState> {
           <div className={'slideItemContainer2 extraItem'}>
           </div>
 
-          <p className={"slidesDesc"}>
-            <Trans i18nKey={"slides.slidesDesc"}>
+          <p className={'slidesDesc'}>
+            <Trans i18nKey={'slides.slidesDesc'}>
               You can download all the presentations <a href={'assets/education/slides_bitcoin-studio.zip'} download>here</a> or
-              consult them on <a href="https://www.slideshare.net/bitcoin-studio" target={"_blank"}>SlideShare</a>.
+              consult them on <a href="https://www.slideshare.net/bitcoin-studio" target={'_blank'}>SlideShare</a>.
             </Trans>
           </p>
         </section>
@@ -288,4 +291,4 @@ class Resources extends React.Component<IProps, IState> {
 }
 
 // @ts-ignore
-export default  withNamespaces('Resources')(Resources)
+export default withNamespaces('Resources')(Resources)
