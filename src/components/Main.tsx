54475080aeb813cc6e30232ffa34a00fd0f244f9
Janaka-Steph <rochestephane@yahoo.fr>
2019-12-25 17:55:46 +0000

    Add css page transition
    But I had to remove lazy loading :(

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,17 +1,72 @@
-import React, {lazy, memo} from 'react'
-import {Route, Switch} from 'react-router-dom'
+import React, {memo} from 'react'
+import {Route} from 'react-router-dom'
+import {
+  CSSTransition,
+} from 'react-transition-group'
+
+import Home from '../pages/Home'
+import Workshops from '../pages/Workshops'
+import Resources from '../pages/Resources'
+import About from '../pages/About'
+import Contact from '../pages/Contact'
+import NotFound from '../pages/NotFound'
+
+const routes = [
+  {path: '/', Component: Home},
+  {path: '/workshops', Component: Workshops},
+  {path: '/resources', Component: Resources},
+  {path: '/about', Component: About},
+  {path: '/contact', Component: Contact},
+]
+
+function SiteRoutes(): any {
+  const routeComponents = routes.map(({path, Component}) => (
+    <Route key={path} exact path={path}>
+      {({match}) => (
+        <CSSTransition
+          in={match != null}
+          timeout={300}
+          classNames="pageContainer"
+          unmountOnExit
+        >
+          <div className="pageContainer">
+            <Component/>
+          </div>
+        </CSSTransition>
+      )}
+    </Route>
+  ))
+
+  /* Hack to have 404 page working with CssTransition */
+  routeComponents.push(
+    <Route key={'NotFound'}>
+      {routeProps => {
+        if (routes.filter(r => r.path === routeProps.location.pathname).length === 0) {
+          return (
+            <CSSTransition
+              in={routeProps.match != null}
+              timeout={300}
+              classNames="pageContainer"
+              unmountOnExit
+            >
+              <div className="pageContainer">
+                <NotFound/>
+              </div>
+            </CSSTransition>
+          )
+        } else {
+          return <></>
+        }
+      }}
+    </Route>,
+  )
+  return routeComponents
+}
 
 function Main() {
   return (
     <main>
-      <Switch>
-        <Route exact path='/' component={lazy(() => import('../pages/Home'))}/>
-        <Route exact path='/workshops' component={lazy(() => import('../pages/Workshops'))}/>
-        <Route exact path='/resources' component={lazy(() => import('../pages/Resources'))}/>
-        <Route exact path='/about' component={lazy(() => import('../pages/About'))}/>
-        <Route exact path='/contact' component={lazy(() => import('../pages/Contact'))}/>
-        <Route path='' component={lazy(() => import('../pages/NotFound'))}/>
-      </Switch>
+      <SiteRoutes/>
     </main>
   )
 }
