51791d37ebb4e5088386a1d6a5bbd656495e80d3
Janaka-Steph <rochestephane@yahoo.fr>
2020-03-30 19:12:07 +0100

    Add namespace

--- deploy.sh
+++ deploy.sh
@@ -19,8 +19,9 @@ echo "## K8S ##"
 echo -n "K8S Context: "
 kubectl config current-context --kubeconfig=${HOME}/.kube/config
 
-# echo "K8S Secrets"
-# kubectl create secret generic smtp-gmail-pass --from-literal SMTP_GMAIL_PASS=$SMTP_GMAIL_PASS --kubeconfig=${HOME}/.kube/config
+###
+# One Time Configuration
+###
 
 # Install nginx-ingress / create load balancer with external IP
 # helm install my-nginx stable/nginx-ingress
@@ -33,19 +34,44 @@ kubectl config current-context --kubeconfig=${HOME}/.kube/config
 # helm install cert-manager jetstack/cert-manager --namespace cert-manager --version v0.14.0
 # kubectl get pods --namespace cert-manager
 
+# echo "K8S Secrets"
+#kubectl create secret generic smtp-gmail-pass --from-literal=SMTP_GMAIL_PASS=$SMTP_GMAIL_PASS \
+#--namespace=bitcoin-studio-prod --kubeconfig=${HOME}/.kube/config
+
+#####
+
 echo "K8S Apply"
-kubectl apply -f ./k8s/bitcoin-studio-server-cluster-ip-service.yaml --kubeconfig=${HOME}/.kube/config
-kubectl apply -f ./k8s/bitcoin-studio-client-cluster-ip-service.yaml --kubeconfig=${HOME}/.kube/config
+kubectl apply -f ./k8s/bitcoin-studio-prod-namespace.yaml \
+--kubeconfig=${HOME}/.kube/config
 
-kubectl apply -f ./k8s/bitcoin-studio-server-deployment.yaml --kubeconfig=${HOME}/.kube/config
-kubectl apply -f ./k8s/bitcoin-studio-client-deployment.yaml --kubeconfig=${HOME}/.kube/config
+kubectl apply -f ./k8s/bitcoin-studio-server-cluster-ip-service.yaml \
+--namespace=bitcoin-studio-prod --kubeconfig=${HOME}/.kube/config
 
-kubectl apply -f ./k8s/certificate.yaml --kubeconfig=${HOME}/.kube/config
-kubectl apply -f ./k8s/issuer.yaml --kubeconfig=${HOME}/.kube/config
-kubectl apply -f ./k8s/bitcoin-studio-ingress-service.yaml --kubeconfig=${HOME}/.kube/config
+kubectl apply -f ./k8s/bitcoin-studio-client-cluster-ip-service.yaml \
+--namespace=bitcoin-studio-prod --kubeconfig=${HOME}/.kube/config
+
+kubectl apply -f ./k8s/bitcoin-studio-server-deployment.yaml \
+--namespace=bitcoin-studio-prod --kubeconfig=${HOME}/.kube/config
+
+kubectl apply -f ./k8s/bitcoin-studio-client-deployment.yaml \
+--namespace=bitcoin-studio-prod --kubeconfig=${HOME}/.kube/config
+
+kubectl apply -f ./k8s/certificate.yaml \
+--namespace=bitcoin-studio-prod --kubeconfig=${HOME}/.kube/config
+
+kubectl apply -f ./k8s/issuer.yaml \
+--namespace=bitcoin-studio-prod --kubeconfig=${HOME}/.kube/config
+
+kubectl apply -f ./k8s/bitcoin-studio-ingress-service.yaml \
+--namespace=bitcoin-studio-prod --kubeconfig=${HOME}/.kube/config
 
 
 echo "K8S Update"
 # https://kubernetes.io/docs/concepts/workloads/controllers/deployment/#updating-a-deployment
-kubectl set image deployments/bitcoin-studio-client-deployment bitcoin-studio-client=janakasteph/bitcoin-studio-client:$SHA --kubeconfig=${HOME}/.kube/config
-kubectl set image deployments/bitcoin-studio-server-deployment bitcoin-studio-server=janakasteph/bitcoin-studio-server:$SHA --kubeconfig=${HOME}/.kube/config
+kubectl set image deployments/bitcoin-studio-client-deployment \
+bitcoin-studio-client=janakasteph/bitcoin-studio-client:$SHA \
+--namespace=bitcoin-studio-prod --kubeconfig=${HOME}/.kube/config
+
+kubectl set image deployments/bitcoin-studio-server-deployment \
+bitcoin-studio-server=janakasteph/bitcoin-studio-server:$SHA \
+--namespace=bitcoin-studio-prod --kubeconfig=${HOME}/.kube/config
