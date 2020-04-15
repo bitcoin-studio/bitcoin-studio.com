a78b572abef46f4a833b1685c3daa275f0b62d6a
Janaka-Steph <rochestephane@yahoo.fr>
2020-04-15 17:32:32 +0100

    WIP

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
 
 ReactDOM.render(
   <BrowserRouter
-    basename={i18n.language === 'fr' ? '/fr' : ''}
+    basename={i18n.language === 'fr' ? '/function/bitcoin-studio-client/fr' : '/function/bitcoin-studio-client'}
   >
     <ScrollToTop/>
     <Suspense fallback={<DelayedFallbackLoader/>}>
