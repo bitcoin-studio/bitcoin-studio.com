b9c821aa2fa3baeda134fa721561a5bd0e4f79bf
stephane <rochestephane@yahoo.fr>
2021-04-01 16:55:36 +0100

    bump year

--- function/src/components/Footer.tsx
+++ function/src/components/Footer.tsx
@@ -65,7 +65,7 @@ export const Footer: React.FC = () => {
 
       <div id="copyrights">
         <div className="strike"/>
-        <p>Bitcoin Studio - 2020</p>
+        <p>Bitcoin Studio - 2021</p>
         <p>SIRET 795 207 117 00016</p>
         <p>
           <Trans i18nKey="license" t={t}>
